758325cea9255ed8abd2e3cbd395ecd9c1f796f3
Sandip Kumar (SW-GPU) <sandikumar@nvidia.com>
2017-08-23 23:43:18 +0530

    Make the code little better to read and remove unnessary import, and make little code little better but logic is same.

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -1,78 +1,68 @@
-import java.util.*;
 
 /*
-
 Given two sequences, find the length of longest subsequence present 
 in both of them. A subsequence is a sequence that appears in the same 
 relative order, but not necessarily contiguous. For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc 
 are subsequences of “abcdefg”. So a string of length n has 2^n different possible subsequences.
-
 */
 
 
-class LongestCommonSubsequence {
+public class LongestCommonSubsequence {
 
 	public static void main(String[] args) {
-		String a = "abcdaf";
-		String b = "acbcf";
+		String str1 = "abcdaf";
+		String str2 = "acbcf";
 
-		char[] str1 = a.toCharArray();
-		char[] str2 = b.toCharArray();
+		int[][] trackMatrix = new int[str1.length()][str2.length()];
+                
+                LongestCommonSubsequence lcm = new LongestCommonSubsequence();
 
-		int[][] trackMatrix = new int[str1.length][str2.length];
-
-		System.out.println(LCM(trackMatrix,str1,str2,str1.length-1,str2.length-1));
-		printSubsequence(trackMatrix,str1,str1.length-1,str2.length-1);
+		System.out.println( lcm.longestCommonSubsequence(trackMatrix,str1,str2,str1.length()-1,str2.length()-1) );
+		lcm.printSubsequence(trackMatrix,str1,str1.length()-1,str2.length()-1 );
 		System.out.println("");
-
-
 	}
 	
-
-	public static int LCM(int[][] trackMatrix, char [] str1, char[] str2, int i, int j)
+	private int longestCommonSubsequence(int[][] trackMatrix, String str1, String str2, int str1Len, int str2Len)
 	{
-		if (i==-1 || j==-1)
+		if (str1Len==-1 || str2Len==-1)
 			return 0;
 
-		if (str1[i]==str2[j]){
-			trackMatrix[i][j] = 0;
-			return 1 + LCM(trackMatrix,str1,str2,i-1,j-1);
+		if (str1.charAt(str1Len) == str2.charAt(str2Len)){
+			trackMatrix[str1Len][str2Len] = 0;
+			return 1 + longestCommonSubsequence(trackMatrix,str1,str2,str1Len-1,str2Len-1);
 		}
 		else{
-			int a = LCM(trackMatrix,str1,str2,i-1,j);
-			int b = LCM(trackMatrix,str1,str2,i,j-1);
+			int len1 = longestCommonSubsequence(trackMatrix,str1,str2,str1Len-1,str2Len);
+			int len2 = longestCommonSubsequence(trackMatrix,str1,str2,str1Len,str2Len-1);
 
-			if (a>b) {
-				trackMatrix[i][j] = -1;
-				return a;
+			if (len1 > len2) {
+				trackMatrix[str1Len][str2Len] = -1;
+				return len1;
 			}
 			else{
-				trackMatrix[i][j]=1;
-				return b;
+				trackMatrix[str1Len][str2Len]=1;
+				return len2;
 			}
 		}
 	}
 
-	public static void printSubsequence(int[][] trackMatrix, char[] str1, int i, int j)
+	private void printSubsequence(int[][] trackMatrix, String str1, int len1, int len2)
 	{
-		if (i==-1 || j==-1) {
+		if (len1==-1 || len2==-1) {
 			return;
 		}
 
-		if (trackMatrix[i][j]==0) {
-			printSubsequence(trackMatrix, str1, i-1,j-1);
-			System.out.print(str1[i]);
-		}
-		else if (trackMatrix[i][j]==-1) {
-			printSubsequence(trackMatrix,str1,i-1,j);
-			
-		}
-		else
-			printSubsequence(trackMatrix,str1,i,j-1);
+            switch (trackMatrix[len1][len2]) {
+                case 0:
+                    printSubsequence(trackMatrix, str1, len1-1,len2-1);
+                    System.out.print(str1.charAt(len1));
+                    break;
+                case -1:
+                    printSubsequence(trackMatrix,str1,len1-1,len2);
+                    break;
+                default:
+                    printSubsequence(trackMatrix,str1,len1,len2-1);
+                    break;
+            }
 	}
-
-	// public static int max(int a, int b)
-	// {
-	// 	return a>b?a:b;
-	// }
 }
